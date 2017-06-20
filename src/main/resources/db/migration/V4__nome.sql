91c272c67fcf9ede826e89a141ce548a2abc467d
Fabio Nogueira de Lucena <fabio@inf.ufg.br>
2017-06-20 15:51:36 -0300

    nome, sobrenome

--- src/main/resources/db/migration/V4__nome.sql
+++ src/main/resources/db/migration/V4__nome.sql
@@ -18,6 +18,23 @@ INSERT INTO UsoCondicional VALUES (6, 'Requisito de privacidade/segurança espec
 INSERT INTO UsoCondicional VALUES (9, 'Nome temporário');
 
 /*
+  LOOKUP TABLE
+  Seção 9.9.2 Uso do nome
+ */
+
+CREATE TABLE UsoDoNome (
+  codigo int,
+  alternativo char,
+  descricao varchar
+);
+
+INSERT INTO UsoDoNome VALUES (1, 'R', 'relatório');
+INSERT INTO UsoDoNome VALUES (2, 'N', 'Nome de recém-nascido');
+INSERT INTO UsoDoNome VALUES (3, 'C', 'Nome profissional ou comercial');
+INSERT INTO UsoDoNome VALUES (4, 'S', 'Nome de solteiro (nome de nascimento) (nome original)');
+INSERT INTO UsoDoNome VALUES (8, 'O', 'Outro nome (alias)');
+
+/*
   Seção 9
   O nome do indivíduo é um elemento de dados composto.
   Pode haver mais de um nome registrado para cada indivíduo. Pelo menos
