2e12afaa4ccde600ad2c798eb6959ed60a8479e6
Fabio Nogueira de Lucena <fabio@inf.ufg.br>
2017-06-20 11:58:20 -0300

    Tipo do identificador

--- src/main/resources/db/migration/V3__identificador_do_individuo.sql
+++ src/main/resources/db/migration/V3__identificador_do_individuo.sql
@@ -1,3 +1,6 @@
+DROP TABLE IF EXISTS AreaGeografica;
+DROP TABLE IF EXISTS IdentificadorDoIndividuo;
+DROP TABLE IF EXISTS TipoDoIdentificador;
 
 /* LOOKUP TABLE
    Um código que representa a área geográfica na qual
@@ -16,15 +19,38 @@ INSERT INTO AreaGeografica VALUES (2, 'A', 'Identificador da área/região/distr
 INSERT INTO AreaGeografica VALUES (3, 'E', 'Identificador do estado/província/território');
 INSERT INTO AreaGeografica VALUES (4, 'N', 'Identificador nacional');
 
+/*
+  LOOKUP TABLE
+  Tipo de identificador usando pela organização, por exemplo,
+  identificador único do paciente, cartão de saúde, cartão da previdência.
+  FONTES ENCONTRADAS:
+  http://www.pmf.sc.gov.br/arquivos/arquivos/pdf/04_01_2010_10.17.42.24125bd84cc115c7ed10f14d211976a2.PDF
+ */
+
+CREATE TABLE TipoDoIdentificador (
+  codigo varchar,
+  descricao varchar,
+  PRIMARY KEY (codigo)
+);
+
+INSERT INTO TipoDoIdentificador VALUES ('01', 'Identificador único dentro da organização');
+INSERT INTO TipoDoIdentificador VALUES ('21', 'radiologia');
+INSERT INTO TipoDoIdentificador VALUES ('22', 'patologia');
 
 CREATE TABLE IdentificadorDoIndividuo (
-  designacao varchar,
-  area int
+  designacao varchar NOT NULL,
+  area int,
+  emissor varchar,
+  dataDeEmissao DATE,
+  tipoDoIdentificador varchar
 );
 
 ALTER TABLE IdentificadorDoIndividuo
-ADD CONSTRAINT FK_AreaGeograficaCodigo
+  ADD CONSTRAINT FK_AreaGeograficaCodigo
 FOREIGN KEY (area) REFERENCES AreaGeografica(codigo);
 
-INSERT INTO IdentificadorDoIndividuo VALUES (1, 1);
-INSERT INTO IdentificadorDoIndividuo VALUES (1, 4);
+ALTER TABLE IdentificadorDoIndividuo
+  ADD CONSTRAINT FK_TipoDoIdentificadorCodigo
+FOREIGN KEY (tipoDoIdentificador) REFERENCES TipoDoIdentificador(codigo);
+
+INSERT INTO IdentificadorDoIndividuo VALUES (1, 1, 'N/A', DATE '2017-12-31', '01');
