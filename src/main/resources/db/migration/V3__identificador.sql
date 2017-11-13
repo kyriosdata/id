b837365d5ec7d66b2249d22e9ba95ba0e5ac8c0d
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2017-11-13 09:17:02 -0200

    revisao

--- src/main/resources/db/migration/V3__identificador.sql
+++ src/main/resources/db/migration/V3__identificador.sql
@@ -1,3 +1,11 @@
+/*
+  O identificador do indivíduo também é conhecido por:
+  (a) ID do paciente; (b) identificador pessoal;
+  (c) número único do prontuário; (d) número do prontuário médico;
+  (e) identificador local do sujeito; (f) número de identificação
+  do sujeito; e (g) identificador único, dentre outros.
+ */
+
 DROP TABLE IF EXISTS IDENTIFICADOR;
 DROP TABLE IF EXISTS CTPS;
 DROP TABLE IF EXISTS CERTIDAO;
@@ -31,14 +39,14 @@ CREATE TABLE TITULOELEITORAL (
 
 ALTER TABLE IDENTIFICADOR
   ADD CONSTRAINT FK_AreaGeograficaCodigo
-FOREIGN KEY (area) REFERENCES AreaGeografica(codigo);
+FOREIGN KEY (area) REFERENCES AREAGEOGRAFICA (codigo);
 
 ALTER TABLE IDENTIFICADOR
   ADD CONSTRAINT FK_TipoDoIdentificadorCodigo
-FOREIGN KEY (tipo) REFERENCES TipoDoIdentificador(codigo);
+FOREIGN KEY (tipo) REFERENCES TIPODOIDENTIFICADOR (codigo);
 
 ALTER TABLE CTPS
   ADD CONSTRAINT FK_EstadoCodigo
-FOREIGN KEY (estado) REFERENCES Estado(codigo);
+FOREIGN KEY (estado) REFERENCES ESTADO (codigo);
 
 INSERT INTO IDENTIFICADOR VALUES ('1', 1, 'N/A', DATE '2017-12-31', '01');
