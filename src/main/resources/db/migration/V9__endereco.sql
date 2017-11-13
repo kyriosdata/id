59a50a1ec94d801d51f4fa171e8c5ceea02d7e99
Fábio Nogueira de Lucena <fabio@inf.ufg.br>
2017-11-13 17:29:55 -0200

    nomes

---
+++ src/main/resources/db/migration/V9__endereco.sql
@@ -0,0 +1,45 @@
+/*
+	Seção 11 (pág. 62) - Endereço do indivíduo
+ */
+
+DROP TABLE IF EXISTS ENDERECO;
+DROP TABLE IF EXISTS LINHAENDERECO;
+
+CREATE TABLE ENDERECO (
+	id VARCHAR(36) NOT NULL,
+	individuo VARCHAR(36) NOT NULL,
+	bairro varchar,
+	distrito varchar,
+	municipio int,
+	estado VARCHAR(2),
+	PRIMARY KEY (id)
+);
+
+ALTER TABLE ENDERECO
+		ADD CONSTRAINT FK_EnderecoDeIndividuo
+FOREIGN KEY (individuo) REFERENCES INDIVIDUO (id);
+
+ALTER TABLE ENDERECO
+		ADD CONSTRAINT FK_CodigoMunicipio
+FOREIGN KEY (municipio) REFERENCES MUNICIPIO (codigo);
+
+ALTER TABLE ENDERECO
+		ADD CONSTRAINT FK_EstadoDoEndereco
+FOREIGN KEY (estado) REFERENCES ESTADO (codigo);
+
+/*
+	Seção 11.2 (pág. 65) - Linha de endereço
+ */
+CREATE TABLE LINHAENDERECO (
+	endereco varchar(36) NOT NULL,
+	linha varchar,
+	ordem int
+);
+
+ALTER TABLE LINHAENDERECO
+		ADD CONSTRAINT FK_LinhaTemEnderecoAssociado
+FOREIGN KEY (endereco) REFERENCES ENDERECO (id);
+
+INSERT INTO INDIVIDUO VALUES ('8d4cd0d3-8996-4812-96b4-48b4f1847ff5');
+INSERT INTO INDIVIDUO VALUES ('530e40d3-9482-4dcb-b5cb-2168c98f3b26');
+INSERT INTO INDIVIDUO VALUES ('18afaf46-abe5-46d8-80df-68a44a367a5d');
