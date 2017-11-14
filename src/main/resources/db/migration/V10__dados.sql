3682c9774b536f6fe21051349ee172ac1eef35f0
kyriosdata <fabio@inf.ufg.br>
2017-11-14 13:39:19 -0200

    dados

---
+++ src/main/resources/db/migration/V10__dados.sql
@@ -0,0 +1,16 @@
+/*
+	Tabela que mantém peo menos uma entrada (registro)
+	para cada indivíduo assistido. Idealmente deveria
+	manter um único registro por indivíduo.
+ */
+
+DROP TABLE IF EXISTS INDIVIDUO;
+
+CREATE TABLE INDIVIDUO (
+	id VARCHAR(36) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+INSERT INTO INDIVIDUO VALUES ('8d4cd0d3-8996-4812-96b4-48b4f1847ff5');
+INSERT INTO INDIVIDUO VALUES ('530e40d3-9482-4dcb-b5cb-2168c98f3b26');
+INSERT INTO INDIVIDUO VALUES ('18afaf46-abe5-46d8-80df-68a44a367a5d');
