4e75b32f4e2bf8fb87fe4abe38faaceff4daee1a
kyriosdata <fabio@inf.ufg.br>
2017-11-14 16:16:38 -0200

    dados

--- src/main/resources/db/migration/V2__esquema.sql
+++ src/main/resources/db/migration/V2__esquema.sql
@@ -3,6 +3,9 @@ DROP TABLE IF EXISTS IDENTIFICADOR;
 DROP TABLE IF EXISTS CTPS;
 DROP TABLE IF EXISTS CERTIDAO;
 DROP TABLE IF EXISTS TITULOELEITORAL;
+DROP TABLE IF EXISTS NOME;
+DROP TABLE IF EXISTS UTILIZACAO;
+DROP TABLE IF EXISTS REPRESENTACAO;
 
 /*
 	Tabela que mantém peo menos uma entrada (registro)
@@ -103,5 +106,84 @@ CREATE TABLE TITULOELEITORAL (
 ALTER TABLE TITULOELEITORAL
 	ADD CONSTRAINT FK_IdentificadorDoTituloEleitoral
 FOREIGN KEY (identificador) REFERENCES IDENTIFICADOR (id);
+
+/*
+  Seção 9 (página 17)
+  O nome do indivíduo é um elemento de dados composto. Um único nome pode
+  conter múltiplos títulos, nomes atribuídos, sobrenomes e sufixos. Cada
+  um desses é registrado por meio de uma única sequência de caracteres e,
+  nesse caso, um espaço em branco é o separador. Por exemplo, em vez de
+  "Prof." e "Doutor" como dois títulos de um único nome, optou-se por
+  "Prof. Doutor".
+
+  Somente um nome pode ser o nome preferido da pessoa em cada instante de
+  tempo. Adicionalmente, para cada nome de um indivíduo tem-se um uso
+  condicional associado.
+
+  Pode haver mais de um nome registrado para cada indivíduo. Pelo menos
+  um nome deve ser capturado.
+ */
+CREATE TABLE NOME (
+	id varchar(36) NOT NULL,
+	individuo varchar(36) NOT NULL,
+	titulos varchar,
+	nomes varchar,
+	sobrenomes varchar,
+	sufixos varchar,
+	preferido int NOT NULL,
+	usoCondicional int
+);
+
+ALTER TABLE NOME
+	ADD CONSTRAINT FK_UsoCondicionalCodigo
+FOREIGN KEY (usoCondicional) REFERENCES USOCONDICIONAL (codigo);
+
+ALTER TABLE NOME
+	ADD CONSTRAINT FK_IndividuoDoNome
+FOREIGN KEY (individuo) REFERENCES INDIVIDUO (id);
+
+ALTER TABLE NOME
+	ADD CONSTRAINT FK_NomePreferido
+FOREIGN KEY (preferido) REFERENCES NOMEPREFERIDO (codigo);
+
+/*
+  Grupo de utilização do nome
+  Seção 9.9 (pág. 40)
+ */
+
+CREATE TABLE UTILIZACAO (
+	nome varchar(36) NOT NULL,
+	uso int,
+	identificador varchar(36) NOT NULL,
+	dataInicial DATE,
+	dataFinal DATE
+);
+
+ALTER TABLE UTILIZACAO
+	ADD CONSTRAINT FK_UtilizacaoDeNome
+FOREIGN KEY (nome) REFERENCES NOME (id);
+
+ALTER TABLE UTILIZACAO
+	ADD CONSTRAINT FK_UtilizacaoExigeIdentificador
+FOREIGN KEY (identificador) REFERENCES IDENTIFICADOR (id);
+
+ALTER TABLE UTILIZACAO
+	ADD CONSTRAINT FK_UsoDoNomeCodigo
+FOREIGN KEY (uso) REFERENCES NOMEUSO (codigo);
+
+/*
+  Representação alternativa de um nome.
+  Seção 9.10 (pág. 43)
+ */
+
+CREATE TABLE REPRESENTACAO (
+	nome varchar(36) NOT NULL,
+	utilizacao varchar,
+	alternativa varchar
+);
+
+ALTER TABLE REPRESENTACAO
+	ADD CONSTRAINT FK_NomeDaRepresentacao
+FOREIGN KEY (nome) REFERENCES NOME (id);
 
 
