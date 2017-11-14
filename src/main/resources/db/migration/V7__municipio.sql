e400fd9dc3fef84fa52e20517e0973fc8b4affe5
kyriosdata <fabio@inf.ufg.br>
2017-11-14 13:42:37 -0200

    dados

--- src/main/resources/db/migration/V7__municipio.sql
+++ src/main/resources/db/migration/V7__municipio.sql
@@ -1,9 +1,49 @@
+
+DROP TABLE IF EXISTS ESTADO;
+DROP TABLE IF EXISTS MUNICIPIO;
+
+/*
+  Seção 10.11 (pág. 58)
+  Unidade da federação.
+ */
+CREATE TABLE ESTADO (
+	codigo varchar(2),
+	nome varchar,
+	PRIMARY KEY (codigo)
+);
+
+INSERT INTO ESTADO VALUES ('AC', 'Acre');
+INSERT INTO ESTADO VALUES ('AL', 'Alagoas');
+INSERT INTO ESTADO VALUES ('AM', 'Amazonas');
+INSERT INTO ESTADO VALUES ('AP', 'Amapá');
+INSERT INTO ESTADO VALUES ('BA', 'Bahia');
+INSERT INTO ESTADO VALUES ('CE', 'Ceará');
+INSERT INTO ESTADO VALUES ('DF', 'Distrito Federal');
+INSERT INTO ESTADO VALUES ('ES', 'Espírito Santo');
+INSERT INTO ESTADO VALUES ('GO', 'Goiás');
+INSERT INTO ESTADO VALUES ('MA', 'Maranhão');
+INSERT INTO ESTADO VALUES ('MG', 'Minas Gerais');
+INSERT INTO ESTADO VALUES ('MS', 'Mato Grosso do Sul');
+INSERT INTO ESTADO VALUES ('MT', 'Mato Grosso');
+INSERT INTO ESTADO VALUES ('PA', 'Pará');
+INSERT INTO ESTADO VALUES ('PB', 'Paraíba');
+INSERT INTO ESTADO VALUES ('PE', 'Pernambuco');
+INSERT INTO ESTADO VALUES ('PI', 'Piauí');
+INSERT INTO ESTADO VALUES ('PR', 'Paraná');
+INSERT INTO ESTADO VALUES ('RJ', 'Rio de Janeiro');
+INSERT INTO ESTADO VALUES ('RN', 'Rio Grande do Norte');
+INSERT INTO ESTADO VALUES ('RS', 'Rio Grande do Sul');
+INSERT INTO ESTADO VALUES ('RO', 'Rondônia');
+INSERT INTO ESTADO VALUES ('RR', 'Roraima');
+INSERT INTO ESTADO VALUES ('SC', 'Santa Catarina');
+INSERT INTO ESTADO VALUES ('SE', 'Sergipe');
+INSERT INTO ESTADO VALUES ('SP', 'São Paulo');
+INSERT INTO ESTADO VALUES ('TO', 'Tocantins');
+
 /*
 	Seção 10.10 (pág. 57)
 	Municípios conforme IBGE (códigos).
  */
-
-DROP TABLE IF EXISTS MUNICIPIO;
 
 CREATE TABLE MUNICIPIO (
 	codigo int,
