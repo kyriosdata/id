b858b7db1ce77b7ef74bd933f7227d73e5aeecd1
kyriosdata <fabio@inf.ufg.br>
2017-11-14 16:19:52 -0200

    dados

--- src/main/resources/db/migration/V2__esquema.sql
+++ src/main/resources/db/migration/V2__esquema.sql
@@ -7,6 +7,7 @@ DROP TABLE IF EXISTS NOME;
 DROP TABLE IF EXISTS UTILIZACAO;
 DROP TABLE IF EXISTS REPRESENTACAO;
 DROP TABLE IF EXISTS VINCULO;
+DROP TABLE IF EXISTS COMUNICACAO;
 
 /*
 	Tabela que mantém peo menos uma entrada (registro)
@@ -214,3 +215,35 @@ FOREIGN KEY (individuo) REFERENCES INDIVIDUO (id);
 ALTER TABLE VINCULO
 	ADD CONSTRAINT FK_TipoDeRelacionamento
 FOREIGN KEY (relacionamento) REFERENCES RELACIONAMENTOTIPO (codigo);
+
+/*
+	Seção 12 (pág. 84) Comunicação eletrônica do indivíduo.
+
+	Seção 12.2 (pág. 85) - meio de comunicação,
+	12.3 (pág. 86) - código de preferência de contato,
+	12.4 (pág. 86) - detalhe (endereço) da comunicação eletrônica e
+	12.5 (pág. 87) - código de utilização do contato
+ */
+CREATE TABLE COMUNICACAO (
+	individuo VARCHAR(36) NOT NULL,
+	meio int NOT NULL,
+	preferencia char(1),
+	detalhe varchar NOT NULL,
+	uso int
+);
+
+ALTER TABLE COMUNICACAO
+	ADD CONSTRAINT FK_IndividuoDaComunicacao
+FOREIGN KEY (individuo) REFERENCES INDIVIDUO (id);
+
+ALTER TABLE COMUNICACAO
+	ADD CONSTRAINT FK_TipoComunicacao
+FOREIGN KEY (meio) REFERENCES COMUNICACAOMEIO (codigo);
+
+ALTER TABLE COMUNICACAO
+	ADD CONSTRAINT FK_HorarioPreferencialParaContato
+FOREIGN KEY (preferencia) REFERENCES COMUNICACAOPREFERENCIA (codigo);
+
+ALTER TABLE COMUNICACAO
+	ADD CONSTRAINT FK_UsoDaComunicacao
+FOREIGN KEY (uso) REFERENCES COMUNICACAOUSO (codigo);
--- src/main/resources/db/migration/V6__comunicacao.sql
+++ src/main/resources/db/migration/V6__comunicacao.sql
@@ -3,37 +3,4 @@
 	contato com o indivíduo.
  */
 
-DROP TABLE IF EXISTS COMUNICACAO;
-
-/*
-	Seção 12 (pág. 84) Comunicação eletrônica do indivíduo.
-
-	Seção 12.2 (pág. 85) - meio de comunicação,
-	12.3 (pág. 86) - código de preferência de contato,
-	12.4 (pág. 86) - detalhe (endereço) da comunicação eletrônica e
-	12.5 (pág. 87) - código de utilização do contato
- */
-CREATE TABLE COMUNICACAO (
-	individuo VARCHAR(36) NOT NULL,
-	meio int NOT NULL,
-	preferencia char(1),
-	detalhe varchar NOT NULL,
-	uso int
-);
-
-ALTER TABLE COMUNICACAO
-		ADD CONSTRAINT FK_IndividuoDaComunicacao
-FOREIGN KEY (individuo) REFERENCES INDIVIDUO (id);
-
-ALTER TABLE COMUNICACAO
-	ADD CONSTRAINT FK_TipoComunicacao
-FOREIGN KEY (meio) REFERENCES COMUNICACAOMEIO (codigo);
-
-ALTER TABLE COMUNICACAO
-	ADD CONSTRAINT FK_HorarioPreferencialParaContato
-FOREIGN KEY (preferencia) REFERENCES COMUNICACAOPREFERENCIA (codigo);
-
-ALTER TABLE COMUNICACAO
-	ADD CONSTRAINT FK_UsoDaComunicacao
-FOREIGN KEY (uso) REFERENCES COMUNICACAOUSO (codigo);
 
