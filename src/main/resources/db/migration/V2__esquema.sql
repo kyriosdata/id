fdf4d9a9da427844ff90ff467eca63d946815b69
kyriosdata <fabio@inf.ufg.br>
2017-11-14 16:21:52 -0200

    dados

--- src/main/resources/db/migration/V2__esquema.sql
+++ src/main/resources/db/migration/V2__esquema.sql
@@ -8,6 +8,7 @@ DROP TABLE IF EXISTS UTILIZACAO;
 DROP TABLE IF EXISTS REPRESENTACAO;
 DROP TABLE IF EXISTS VINCULO;
 DROP TABLE IF EXISTS COMUNICACAO;
+DROP TABLE IF EXISTS DADODEMOGRAFICO;
 
 /*
 	Tabela que mantém peo menos uma entrada (registro)
@@ -247,3 +248,78 @@ FOREIGN KEY (preferencia) REFERENCES COMUNICACAOPREFERENCIA (codigo);
 ALTER TABLE COMUNICACAO
 	ADD CONSTRAINT FK_UsoDaComunicacao
 FOREIGN KEY (uso) REFERENCES COMUNICACAOUSO (codigo);
+
+/*
+	Seção 10 (pág. 44)
+	Dados demográficos adicionais.
+ */
+CREATE TABLE DADODEMOGRAFICO (
+	individuo VARCHAR(36) NOT NULL,
+	nascimento DATE,
+	nascimentoAcuracia varchar(3),
+	nascimentoSeguimento char,
+	nascimentoPluralidade int,
+	nascimentoOrdem int,
+	obito DATE,
+	obitoAcuracia varchar(3),
+	obitoFonte int,
+	sexo int NOT NULL,
+	mae varchar,
+	pai varchar,
+	situacaoFamiliar int,
+	raca int,
+	comentario varchar,
+	nacionalidade int,
+	municipio int,
+	estado varchar(2),
+	pais int,
+	dataEntradaPais DATE
+);
+
+ALTER TABLE DADODEMOGRAFICO
+	ADD CONSTRAINT FK_IndividuoDoDadoDemografico
+FOREIGN KEY (individuo) REFERENCES INDIVIDUO (id);
+
+ALTER TABLE DADODEMOGRAFICO
+	ADD CONSTRAINT FK_PluralidadeDoNascimento
+FOREIGN KEY (nascimentoPluralidade) REFERENCES NASCIMENTOPLURALIDADE (codigo);
+
+ALTER TABLE DADODEMOGRAFICO
+	ADD CONSTRAINT FK_OrdemDoNascimento
+FOREIGN KEY (nascimentoOrdem) REFERENCES NASCIMENTOORDEM (codigo);
+
+ALTER TABLE DADODEMOGRAFICO
+	ADD CONSTRAINT FK_DataDemandaSeguimento
+FOREIGN KEY (nascimentoSeguimento) REFERENCES DATASEGUIMENTO (codigo);
+
+ALTER TABLE DADODEMOGRAFICO
+	ADD CONSTRAINT FK_FonteDoObito
+FOREIGN KEY (obitoFonte) REFERENCES OBITOFONTE (codigo);
+
+ALTER TABLE DADODEMOGRAFICO
+	ADD CONSTRAINT FK_Sexo
+FOREIGN KEY (sexo) REFERENCES SEXO (codigo);
+
+ALTER TABLE DADODEMOGRAFICO
+	ADD CONSTRAINT FK_SituacaoFamiliar
+FOREIGN KEY (situacaoFamiliar) REFERENCES SITUACAOFAMILIAR (codigo);
+
+ALTER TABLE DADODEMOGRAFICO
+	ADD CONSTRAINT FK_Raca
+FOREIGN KEY (raca) REFERENCES RACA (codigo);
+
+ALTER TABLE DADODEMOGRAFICO
+	ADD CONSTRAINT FK_Nacionalidade
+FOREIGN KEY (nacionalidade) REFERENCES NACIONALIDADE (codigo);
+
+ALTER TABLE DADODEMOGRAFICO
+	ADD CONSTRAINT FK_ReferenciaParaMunicipio
+FOREIGN KEY (municipio) REFERENCES MUNICIPIO (codigo);
+
+ALTER TABLE DADODEMOGRAFICO
+	ADD CONSTRAINT FK_Estado
+FOREIGN KEY (estado) REFERENCES ESTADO (codigo);
+
+ALTER TABLE DADODEMOGRAFICO
+	ADD CONSTRAINT FK_Pais
+FOREIGN KEY (pais) REFERENCES PAIS (codigo);
