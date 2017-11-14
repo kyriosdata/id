ed47a8959c37295670202b1d4e3905ab05fd6d32
kyriosdata <fabio@inf.ufg.br>
2017-11-14 16:40:21 -0200

    dados

--- src/main/resources/db/migration/V10__dados.sql
+++ src/main/resources/db/migration/V10__dados.sql
@@ -9,29 +9,43 @@ INSERT INTO INDIVIDUO VALUES ('18afaf46-abe5-46d8-80df-68a44a367a5d');
 
 
 INSERT INTO IDENTIFICADOR (id, individuo, designacao, area, emissor, data, tipo)
-VALUES ('7ec971f2-bd30-4704-8952-d705e9b006e5', '8d4cd0d3-8996-4812-96b4-48b4f1847ff5',
-				12345678, 1, 'N/A', DATE '2017-12-31', '10');
+VALUES ('7ec971f2-bd30-4704-8952-d705e9b006e5',
+        '8d4cd0d3-8996-4812-96b4-48b4f1847ff5',
+        12345678, 1, 'N/A', DATE '2017-12-31', '10');
 
-
+/* Zuza do Luar é o nome preferido de José Pedro dos Santos Filho */
 INSERT INTO NOME (
-	id, individuo,
-	titulos, nomes, sobrenomes, sufixos, preferido, usoCondicional)
+  id, individuo,
+  titulos, nomes, sobrenomes, sufixos, preferido)
 VALUES (
-	'8db412b5-bc25-4ced-96ba-2014d1178550', '8d4cd0d3-8996-4812-96b4-48b4f1847ff5',
-	NULL, 'José Pedro', 'dos Santos', 'Filho', 1, 3);
+  '',
+  '8d4cd0d3-8996-4812-96b4-48b4f1847ff5',
+  NULL, 'José Pedro', 'dos Santos', 'Filho', 2);
+INSERT INTO NOME (
+  id, individuo,
+  titulos, nomes, sobrenomes, sufixos, preferido)
+VALUES (
+  '8db412b5-bc25-4ced-96ba-2014d1178550',
+  '8d4cd0d3-8996-4812-96b4-48b4f1847ff5',
+  NULL, 'José Pedro', 'dos Santos', 'Filho', 2);
 
-INSERT INTO VINCULO VALUES ('7ec971f2-bd30-4704-8952-d705e9b006e5', '8d4cd0d3-8996-4812-96b4-48b4f1847ff5', 41, DATE '2016-12-31', DATE '2017-12-31');
+INSERT INTO VINCULO
+(IDENTIFICADOR, INDIVIDUO, RELACIONAMENTO, DATAINICIO, DATAFINAL)
+VALUES ('7ec971f2-bd30-4704-8952-d705e9b006e5',
+        '8d4cd0d3-8996-4812-96b4-48b4f1847ff5', 41,
+        DATE '2016-12-31', DATE '2017-12-31');
 
-INSERT INTO COMUNICACAO VALUES ('8d4cd0d3-8996-4812-96b4-48b4f1847ff5', 4, 'B', 'f@g', 1);
+INSERT INTO COMUNICACAO (INDIVIDUO, MEIO, PREFERENCIA, DETALHE, USO)
+VALUES ('8d4cd0d3-8996-4812-96b4-48b4f1847ff5', 4, 'B', 'f@gmail.com', 1);
 
-
-INSERT INTO DADODEMOGRAFICO(
-	individuo,
-	nascimento, nascimentoAcuracia, nascimentoSeguimento, nascimentoPluralidade, nascimentoOrdem,
-	obito, obitoAcuracia, obitoFonte, sexo, mae, pai, situacaoFamiliar, raca, comentario,
-	nacionalidade, municipio, estado, pais, dataEntradaPais) VALUES (
-	'8d4cd0d3-8996-4812-96b4-48b4f1847ff5',
-	DATE '2017-11-13', 'AEU', 'S', 1, 1,
-	DATE '2017-11-14', 'AAA', 1, 1, 'nome da mãe', 'nome do pai',	1, 1, 'um comentário',
-	1, 1100015, 'RO', NULL, NULL
+INSERT INTO DADODEMOGRAFICO (
+  individuo,
+  nascimento, nascimentoAcuracia, nascimentoSeguimento, nascimentoPluralidade, nascimentoOrdem,
+  obito, obitoAcuracia, obitoFonte, sexo, mae, pai, situacaoFamiliar, raca, comentario,
+  nacionalidade, municipio, estado, pais, dataEntradaPais) VALUES (
+  '8d4cd0d3-8996-4812-96b4-48b4f1847ff5',
+  DATE '2017-11-13', 'AEU', 'S', 1, 1,
+  DATE '2017-11-14', 'AAA', 1, 1, 'nome da mãe', 'nome do pai', 1, 1,
+  'um comentário',
+  1, 1100015, 'RO', NULL, NULL
 );
